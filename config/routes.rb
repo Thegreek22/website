8e7e661a5af01ee44746a6c8bfb666fa44e7cd4d
Ivan Garcia <vavimayor159@gmail.com>
2015-03-24 14:30:58 -0600

    gema de devise funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  Rails.application.routes.draw do
-        devise_for :users, controllers: {
-          sessions: 'sessions'
-        }
-      end
+  # Rails.application.routes.draw do
+  #       devise_for :users, controllers: {
+  #         sessions: 'sessions'
+  #       }
+  #     end
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
