8c5b5654328b2a778b119ded9af269882411e8a8
Ivan Garcia <vavimayor159@gmail.com>
2015-04-04 15:05:25 -0600

    profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   mount_uploader :image, ImageUploader
+
+  def user_photo
+  	@user = User.where(id: self.user_id)[0]
+  	return @user.photo
+  end
 end
