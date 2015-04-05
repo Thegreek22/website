693c57db634cf3aac5beef8b966dae5b985e4b48
Ivan Garcia <vavimayor159@gmail.com>
2015-04-04 21:21:18 -0600

    prof

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,15 @@
 class Category < ActiveRecord::Base
 	has_many :posts
+	def photo
+		@categoria_actual = case self.id
+                          when "1"
+                            "SociedadTecnología.jpg"
+                          when "2"
+                            "NuevosParadigmas.jpg"
+                          when "3"
+                            "DesarrolloHumano.jpg"
+                          when "4"
+                            "ComportamientoTecnologico.jpg"
+                        end
+	end
 end
