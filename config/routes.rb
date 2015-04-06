89835790ec3231c033baa4694eea5f72702bec80
Ivan Garcia <vavimayor159@gmail.com>
2015-04-05 20:13:38 -0500

    mergeback

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :posts
-  get 'perfil' => 'post#perfil'
+  get 'perfil' => 'posts#perfil'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
